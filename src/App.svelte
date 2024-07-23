6ce6641cfee1f98dc41498999c7b647662e69c50
Sander Kools <s.w.a.kools@minezk.nl>
2024-07-23 16:11:30 +0200

    created seperate hexagon object

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,35 @@
 <script>
 	import { onMount } from "svelte";
+    import Hexagon from "./Hexagon.svelte";
 
 	// A box and corresponding width and height value of the box.
 	// The box is set to the full screen width and height 
 	// and we observe the box for any changes in the size
 	let screenBox;
-	let screenWidth = 0;
-	let screenHeight = 0;
-
-	// The internal dimensions of the svg viewBox. 
-	const width = 100;
-	const height = 100;
-
-	// Actual size of the hexagon on the screen
-	const hexWidth = 100;
-	const hexHeight = 100;
+	let screenWidth = -1;
+	let screenHeight = -1;
 	
-	const sidesOfHexagon = 6;
-	const radius = width/2;
-    const angle = (Math.PI * 2) / sidesOfHexagon;
-
-	const firstPointX = radius * Math.cos(0.0) + (width / 2);
-	const firstPointY = radius * Math.sin(0.0) + (height / 2);
-
-    var points = [];
-	points.push([firstPointX, firstPointY]);
-	for (let i = 1; i <= sidesOfHexagon; i++) {
-		const x = radius * Math.cos(angle * i) + (width / 2);
-		const y = radius * Math.sin(angle * i) + (height / 2);
-		points.push([x, y]);
-    }
-
-	var polygonColour = "#0000FFFF";
-
-	function handleClick(e) {
-		console.log("click event: " + e);
-		polygonColour = "#FF0000FF";
-	}
-
-	// Add the observer when component mounts and cleanup after
 	onMount(() => {
 		const resizeObserver = new ResizeObserver(entries => {
-			// We're only watching one element
 			const entry = entries.at(0);
 
-			//Get the block size
 			screenWidth = entry.contentRect.width;
 			screenHeight = entry.contentRect.height;
 		});
 		resizeObserver.observe(screenBox);
-	
-		// This callback cleans up the observer
 		return () => resizeObserver.unobserve(screenBox);
 	});
+
+	// Not the cleanest solution, but we will manually define all the hexagons that we want to display.
+	// We will use the q, r, s coordinates system even though just q, r should be sufficient.
+	const hexagons = [];
+	hexagons.push([0, 0, 0]);
+	// hexagons.push([0, -1, 1]);
+	// hexagons.push([1, -1, 0]);
+	// hexagons.push([1, 0, -1]);
+	// hexagons.push([0, 1, -1]);
+	// hexagons.push([-1, 1, 0]);
+	// hexagons.push([-1, 0, 1]);
 </script>
 
 <main>
@@ -60,45 +37,12 @@
 	<div class="screen_box" bind:this={screenBox}></div>
 </main>
 
-<div class="hex_item">
-	<svg id="hex_svg" class="svg_style" width={{hexWidth}} height={{hexHeight}} viewBox="0 0 {width} {height}">
-		<g id=hex_button fill="{polygonColour}" on:click={handleClick} on:keydown={handleClick}>
-			<polygon stroke-width=0 points="
-																{points[0][0]},{points[0][1]}
-																{points[1][0]},{points[1][1]}
-																{points[2][0]},{points[2][1]}
-																{points[3][0]},{points[3][1]}
-																{points[4][0]},{points[4][1]}
-																{points[5][0]},{points[5][1]}">
-			</polygon>
-			<!-- We add an image to the svg. We have a placeholder image and we set the height to half of the svg viewbox
-				We position the x and y start of the image 1/4 of the width and height, 
-				which will position the center of the image in the center of the svg. -->
-			<image href="https://via.placeholder.com/100" height="{height/2}" width="{width/2}" x="{width/4}" y="{height/4}"/>
-		</g>
-	</svg>
-</div>
+{#each hexagons as hexagon}
+	<Hexagon q={hexagon[0]} r={hexagon[1]} s={hexagon[2]}></Hexagon>
+{/each}
+
 
 <style>
-	.hex_item {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
-	.hex_item image {
-	}
-	.hex_item svg {
-	}
-
-	#hex_button {
-		cursor: pointer;
-		fill: #900;
-	}
-	#hex_button:hover {
-		cursor: pointer;
-		fill: #F00;
-	}
 
 	main {
 		font-family: sans-serif;
@@ -115,6 +59,5 @@
 	.screen_box {
 		width: 100vw;
 		height: 100vh;
-		background-color: green;
 	}
 </style>
