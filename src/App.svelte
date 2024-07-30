28bb235d7ae8aafa8ada4ba2a0e60177b317a6b7
Sander Kools <s.w.a.kools@minezk.nl>
2024-07-30 15:42:48 +0200

    updating and resizing hexagons

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,176 @@
 <script>
 	import { onMount } from "svelte";
     import Hexagon from "./Hexagon.svelte";
-
+		
 	// A box and corresponding width and height value of the box.
 	// The box is set to the full screen width and height 
 	// and we observe the box for any changes in the size
 	let screenBox;
 	let screenWidth = -1;
 	let screenHeight = -1;
-	
+	let hexSize = -1;
+
+	// The hexagons list has the attributes for the Hexagons and the hex list will hold the objects.
+	let hexagons = [];
+    let hex = [];
 	onMount(() => {
 		const resizeObserver = new ResizeObserver(entries => {
 			const entry = entries.at(0);
 
 			screenWidth = entry.contentRect.width;
 			screenHeight = entry.contentRect.height;
+			if (screenWidth > screenHeight) {
+				hexSize = screenWidth / 22;
+			} else {
+				hexSize = screenHeight / 22;
+			}
+			for (let i = 0; i <= hex.length - 1; i++) {
+				hex[i].updateHexagon(hexSize);
+			}
 		});
 		resizeObserver.observe(screenBox);
 		return () => resizeObserver.unobserve(screenBox);
 	});
 
 	// Not the cleanest solution, but we will manually define all the hexagons that we want to display.
+	// This gives us full control to the final visual result.
 	// We will use the q, r, s coordinates system even though just q, r should be sufficient.
-	const hexagons = [];
-	hexagons.push([0, 0, 0]);
+	// It should hold for all hexagons that q + r + s == 0
+	// The center hexagon
+	hexagons.push([0, 0, hexSize]);
+	// The first layer (6 neighbours of the center)
 	hexagons.push([0, -1, 1]);
 	hexagons.push([1, -1, 0]);
 	hexagons.push([1, 0, -1]);
 	hexagons.push([0, 1, -1]);
 	hexagons.push([-1, 1, 0]);
 	hexagons.push([-1, 0, 1]);
+	// The second layer
+	hexagons.push([0, -2, 2]);
+	hexagons.push([1, -2, 1]);
+	hexagons.push([2, -2, 0]);
+	hexagons.push([2, -1, -1]);
+	hexagons.push([2, 0, -2]);
+	hexagons.push([1, 1, -2]);
+	hexagons.push([0, 2, -2]);
+	hexagons.push([-1, 2, -1]);
+	hexagons.push([-2, 2, 0]);
+	hexagons.push([-2, 1, 1]);
+	hexagons.push([-2, 0, 2]);
+	hexagons.push([-1, -1, 2]);
+	// The third layer
+	hexagons.push([0, -3, 3]);
+	hexagons.push([1, -3, 2]);
+	hexagons.push([2, -3, 1]);
+	hexagons.push([3, -3, 0]);
+	hexagons.push([3, -2, -1]);
+	hexagons.push([3, -1, -2]);
+	hexagons.push([3, 0, -3]);
+	hexagons.push([2, 1, -3]);
+	hexagons.push([1, 2, -3]);
+	hexagons.push([0, 3, -3]);
+	hexagons.push([-1, 3, -2]);
+	hexagons.push([-2, 3, -1]);
+	hexagons.push([-3, 3, 0]);
+	hexagons.push([-3, 2, 1]);
+	hexagons.push([-3, 1, 2]);
+	hexagons.push([-3, 0, 3]);
+	hexagons.push([-2, -1, 3]);
+	hexagons.push([-1, -2, 3]);
+	// The fourth layer
+	hexagons.push([0, -4, 4]);
+	hexagons.push([1, -4, 3]);
+	hexagons.push([2, -4, 2]);
+	hexagons.push([3, -4, 1]);
+	hexagons.push([4, -4, 0]);
+	hexagons.push([4, -3, -1]);
+	hexagons.push([4, -2, -2]);
+	hexagons.push([4, -1, -3]);
+	hexagons.push([4, 0, -4]);
+	hexagons.push([3, 1, -4]);
+	hexagons.push([2, 2, -4]);
+	hexagons.push([1, 3, -4]);
+	hexagons.push([0, 4, -4]);
+	hexagons.push([-1, 4, -3]);
+	hexagons.push([-2, 4, -2]);
+	hexagons.push([-3, 4, -1]);
+	hexagons.push([-4, 4, 0]);
+	hexagons.push([-4, 3, 1]);
+	hexagons.push([-4, 2, 2]);
+	hexagons.push([-4, 1, 3]);
+	hexagons.push([-4, 0, 4]);
+	hexagons.push([-3, -1, 4]);
+	hexagons.push([-2, -2, 4]);
+	hexagons.push([-1, -3, 4]);
+	// The fifth layer
+	hexagons.push([0, -5, 5]);
+	hexagons.push([1, -5, 4]);
+	hexagons.push([2, -5, 3]);
+	hexagons.push([3, -5, 2]);
+	hexagons.push([4, -5, 1]);
+	hexagons.push([5, -5, 0]);
+	hexagons.push([5, -4, -1]);
+	hexagons.push([5, -3, -2]);
+	hexagons.push([5, -2, -3]);
+	hexagons.push([5, -1, -4]);
+	hexagons.push([5, 0, -5]);
+	hexagons.push([4, 1, -5]);
+	hexagons.push([3, 2, -5]);
+	hexagons.push([2, 3, -5]);
+	hexagons.push([1, 4, -5]);
+	hexagons.push([0, 5, -5]);
+	hexagons.push([-1, 5, -4]);
+	hexagons.push([-2, 5, -3]);
+	hexagons.push([-3, 5, -2]);
+	hexagons.push([-4, 5, -1]);
+	hexagons.push([-5, 5, 0]);
+	hexagons.push([-5, 4, 1]);
+	hexagons.push([-5, 3, 2]);
+	hexagons.push([-5, 2, 3]);
+	hexagons.push([-5, 1, 4]);
+	hexagons.push([-5, 0, 5]);
+	hexagons.push([-4, -1, 5]);
+	hexagons.push([-3, -2, 5]);
+	hexagons.push([-2, -3, 5]);
+	hexagons.push([-1, -4, 5]);
+	// The sixth layer
+	hexagons.push([0, -6, 6]);
+	hexagons.push([1, -6, 5]);
+	hexagons.push([2, -6, 4]);
+	hexagons.push([3, -6, 3]);
+	hexagons.push([4, -6, 2]);
+	hexagons.push([5, -6, 1]);
+	hexagons.push([6, -6, 0]);
+	hexagons.push([6, -5, -1]);
+	hexagons.push([6, -4, -2]);
+	hexagons.push([6, -3, -2]);
+	hexagons.push([6, -2, -4]);
+	hexagons.push([6, -1, -5]);
+	hexagons.push([6, 0, -6]);
+	hexagons.push([5, 1, -6]);
+	hexagons.push([4, 2, -6]);
+	hexagons.push([3, 3, -6]);
+	hexagons.push([2, 4, -6]);
+	hexagons.push([1, 5, -6]);
+	hexagons.push([0, 6, -6]);
+	hexagons.push([-1, 6, -5]);
+	hexagons.push([-2, 6, -4]);
+	hexagons.push([-3, 6, -3]);
+	hexagons.push([-4, 6, -2]);
+	hexagons.push([-5, 6, -1]);
+	hexagons.push([-6, 6, 0]);
+	hexagons.push([-6, 5, 1]);
+	hexagons.push([-6, 4, 2]);
+	hexagons.push([-6, 3, 3]);
+	hexagons.push([-6, 2, 4]);
+	hexagons.push([-6, 1, 5]);
+	hexagons.push([-6, 0, 6]);
+	hexagons.push([-5, -1, 6]);
+	hexagons.push([-4, -2, 6]);
+	hexagons.push([-3, -3, 6]);
+	hexagons.push([-2, -4, 6]);
+	hexagons.push([-1, -5, 6]);
+
 </script>
 
 <main>
@@ -37,16 +178,29 @@
 	<div class="screen_box" bind:this={screenBox}></div>
 </main>
 
-{#each hexagons as hexagon}
-	<Hexagon q={hexagon[0]} r={hexagon[1]} s={hexagon[2]}></Hexagon>
+{#each hexagons as hexagon, i}
+	<!-- hexagon -->
+	<Hexagon q={hexagon[0]} r={hexagon[1]} hexSize={hexSize} bind:this={hex[i]}></Hexagon>
 {/each}
 
+<svelte:window on:wheel|nonpassive|preventDefault />
 
 <style>
 
 	main {
 		font-family: sans-serif;
 		text-align: center;
+
+		overflow: auto;
+		/* this will hide the scrollbar in mozilla based browsers */
+		overflow: -moz-scrollbars-none;
+		scrollbar-width: none;
+		/* this will hide the scrollbar in internet explorers */
+		-ms-overflow-style: none;
+	}
+	main::-webkit-scrollbar { 
+		width: 0 !important;
+		display: none;
 	}
 
 	/* the way we detect the screen size. 
@@ -57,6 +211,7 @@
 		padding: 0;
 	}
 	.screen_box {
+		background-color: cadetblue;
 		width: 100vw;
 		height: 100vh;
 	}
--- src/Hexagon.svelte
+++ src/Hexagon.svelte
@@ -1,64 +1,69 @@
 <script>
 	export let q;
 	export let r;
-	export let s;
+	export let hexSize;
 
-	const hexSize = 100;
-	// The internal dimensions of the svg viewBox. 
-	const xSize = 2 * hexSize;
-	const ySize = Math.sqrt(3) * hexSize;
+	// Size of the hexagon and the internal dimensions of the svg viewBox. 
+	let xSize = 2 * hexSize;
+	let ySize = Math.sqrt(3) * hexSize;
 
-	// Actual size of the hexagon on the screen
-	const hexWidth = 2 * hexSize;
-	const hexHeight = Math.sqrt(3) * hexSize;
-	
 	const sidesOfHexagon = 6;
-	const radius = xSize/2;
+	let radius = xSize/2;
     const angle = (Math.PI * 2) / sidesOfHexagon;
-
-	const firstPointX = radius * Math.cos(0.0) + (xSize / 2);
-	const firstPointY = radius * Math.sin(0.0) + (ySize / 2);
-
-	// These hexagon points will be the same for all hexagons within their own viewbox dimensions.
-    var points = [];
-	points.push([firstPointX, firstPointY]);
-	for (let i = 1; i <= sidesOfHexagon; i++) {
-		const x = radius * Math.cos(angle * i) + (xSize / 2);
-		const y = radius * Math.sin(angle * i) + (ySize / 2);
-		points.push([x, y]);
-    }
-
-	var polygonColour = "#0000FFFF";
 
 	let pos_x = 0;
 	let pos_y = 0;
+	function getTilePos() {
+		pos_x = xSize * 3 / 4 * q;
+		const yTr1 = ySize / 2 * q;
+		const yTr2 = ySize * r;
+		pos_y = yTr1 + yTr2;
+	}
+
+	// These hexagon points will be the same for all hexagons within their own viewbox dimensions.
+	var points = [];
+	var polygonColour = "#0000FFFF";
+	function setHexagonDetails() {
+		console.log("setting hex details with size: " + hexSize);
+		// Size of the hexagon and the internal dimensions of the svg viewBox. 
+		xSize = 2 * hexSize;
+		ySize = Math.sqrt(3) * hexSize;
+
+		radius = xSize/2;
+
+		const firstPointX = radius * Math.cos(0.0) + (xSize / 2);
+		const firstPointY = radius * Math.sin(0.0) + (ySize / 2);
+
+		points = [];
+		points.push([firstPointX, firstPointY]);
+		for (let i = 1; i <= sidesOfHexagon; i++) {
+			const x = radius * Math.cos(angle * i) + (xSize / 2);
+			const y = radius * Math.sin(angle * i) + (ySize / 2);
+			points.push([x, y]);
+		}
+		polygonColour = "#0000FFFF";
+	}
+	setHexagonDetails();
+	getTilePos();
+
 	function handleClick(e) {
 		console.log("click event: " + e);
 		polygonColour = "#FF0000FF";
 	}
 
-	function getTilePos(hexQ, hexR) {
-		pos_x = hexSize * 3 / 2 * hexQ - hexSize;
-		console.log(Math.sqrt(3));
-		const yTr1 = hexSize * (Math.sqrt(3) / 2 * hexQ);
-		const yTr2 = hexSize * (Math.sqrt(3) * hexR)
-		pos_y = yTr1 + yTr2;
-
-		// slight offset to put the center in the center and not a corner.
-		pos_x += hexSize;
-
-		console.log("getting tile position");
-		console.log(pos_x);
-		console.log(pos_y);
-	}
-	getTilePos(q, r);
+	export const updateHexagon = function(hexagonSize) {
+        console.log("updateHexagon: " + hexagonSize);
+		hexSize = hexagonSize;
+		setHexagonDetails();
+		getTilePos();
+    }
 
 </script>
 
 <div class="hex_item" style='--pos_x:{pos_x};--pos_y:{pos_y};'>
-    <svg id="hex_svg" class="svg_style" width={hexWidth} height={hexHeight} viewBox="0 0 {xSize} {ySize}">
+    <svg id="hex_svg" class="svg_style" width={xSize} height={ySize} viewBox="0 0 {xSize} {ySize}">
         <g id=hex_button fill="{polygonColour}" on:click={handleClick} on:keydown={handleClick}>
-            <polygon stroke-width=0 points="
+            <polygon class="hex_polygon" points="
                                             {points[0][0]},{points[0][1]}
                                             {points[1][0]},{points[1][1]}
                                             {points[2][0]},{points[2][1]}
@@ -76,21 +81,33 @@
 
 <style>
 	.hex_item {
+		pointer-events: None;
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		transform: 
-		translate(
-			-50%,
-			-50%
-			) 
-		translate(
-			calc( var(--pos_x) * 1px ),
-			calc( var(--pos_y) * 1px )
-			);
+			translate(
+				-50%,
+				-50%
+				)
+			translate(
+				calc( var(--pos_x) * 1px ),
+				calc( var(--pos_y) * 1px )
+				);
+	}
+
+	.svg_style {
+		pointer-events: None;
+	}
+
+	.hex_polygon {
+		pointer-events: auto;
 	}
 
 	#hex_button {
+		pointer-events: None;
+		stroke: chartreuse;
+		stroke-width: 2;
 		cursor: pointer;
 		fill: #900;
 	}
@@ -98,10 +115,4 @@
 		cursor: pointer;
 		fill: #F00;
 	}
-
-	/* div {
-		width : calc( var(--pos_x) * 1px );
-		height : calc( var(--pos_y) * 1px );
-		background-color : green;
-	} */
 </style>
